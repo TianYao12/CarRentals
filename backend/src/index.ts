583e966b12cd065b5eaa39bd65edb02977219d73
TianYao12 <110309420+TianYao12@users.noreply.github.com>
2024-01-23 22:35:47 -0500

    added basic layout

---
+++ backend/src/index.ts
@@ -0,0 +1,19 @@
+import express, {Request, Response} from 'express';
+import cors from 'cors';
+import "dotenv/config";
+import mongoose from 'mongoose'
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
+
+const app = express();
+app.use(express.json()) // converts body of API requests to json
+app.use(express.urlencoded({extended: true})) // helps parse URL
+app.use(cors()) 
+
+app.get("/api/test", async(req: Request, res: Response)=> {
+    res.json({message:"hello hello goodie goodie"})
+});
+
+app.listen(3000, ()=> {
+    console.log("server running on port 3000")
+});
---
+++ frontend/src/components/Hero.tsx
@@ -0,0 +1,16 @@
+const Hero = () => {
+    return(
+        <div className="bg-blue-900 pb-16">
+            <div className="container mx-auto flex flex-col gap-2">
+                <h1 className="text-5xl text-white font-bold">
+                    Find a car
+                </h1>
+                <p className="text-2xl text-white">
+                    Search for low prices on your favourite cars
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Hero;
